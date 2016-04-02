e63ca8ec867dffaa383276f724b9ad0067993e19
versus <dcversus@gmail.com>
2016-04-02 23:40:51 +0300

    #2 deploy notify about ftp-deploy status

--- deploy.js
+++ deploy.js
@@ -2,16 +2,20 @@ var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-    host: process.env.ftp_host,
-    port: 21,
-    username: process.env.ftp_user,
-    password: process.env.ftp_password,
-    localRoot: __dirname,
-    remoteRoot: '',
-    exclude: ['.git', 'tmp/*']
+  host: process.env.ftp_host,
+  port: 21,
+  username: process.env.ftp_user,
+  password: process.env.ftp_password,
+  localRoot: __dirname,
+  remoteRoot: '',
+  exclude: ['.git', 'tmp/*']
 };
 
-ftpDeploy.deploy(config, function (err, fileName) {
-    if (err) console.log("error " + err);
-    else console.log("Completed uploading");
+ftpDeploy.deploy(config, (err, fileName) => {
+  if (err) console.log("error " + err);
+  else console.log("Completed uploading");
+});
+
+ftpDeploy.on('uploading', (data) => {
+  console.log(data.percentComplete);
 });

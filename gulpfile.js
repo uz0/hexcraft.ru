f1c0b4eb0d13c295d278043c84fb09939d4a8453
versus <dcversus@gmail.com>
2016-04-05 14:04:44 +0300

    #2 gulp tasks: static copy, game workspace work, tasks group, jshint for api

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,70 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const jshint = require('gulp-jshint');
-const concat = require('gulp-concat');
-const jade = require('gulp-jade');
-const marked = require('marked');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const nodemon = require('gulp-nodemon');
-const glob = require('glob');
-const Path = require('path');
+const jshint       = require('gulp-jshint');
+const concat       = require('gulp-concat');
+const jade         = require('gulp-jade');
+const marked       = require('marked');
+const sourcemaps   = require('gulp-sourcemaps');
+const babel        = require('gulp-babel');
+const nodemon      = require('gulp-nodemon');
+const glob         = require('glob');
 
-const path = {
+const site = {
   styles: {
     from: 'src/site/stylesheets/**/*.scss',
-    to: 'public/stylesheets',
-    end: 'public/stylesheets/*.css'
+    to:   'public/stylesheets',
+    end:  'public/stylesheets/*.css'
   },
   scripts: {
     from: 'src/site/javascripts/**/*.js',
-    to: 'public/javascripts'
+    to:   'public/javascripts'
   },
   templates: {
     from: 'src/site/templates/**/*.jade',
-    exclude: ['!**/layout.jade', '!**/_header.jade', '!**/_footer.jade'],
-    to: 'public/'
+    ex:   [
+      '!**/layout.jade',
+      '!**/_header.jade',
+      '!**/_footer.jade'
+    ],
+    to:   'public/'
+  },
+  assets: {
+    from: 'src/site/**/*.*',
+    ex:   [
+      '!src/site/**/*.scss',
+      '!src/site/**/*.js',
+      '!src/site/**/*.jade',
+      '!src/site/**/*.md'
+    ],
+    to:   'public/'
   }
 }
 
+const api = {
+  scripts: 'src/api/**/*.js'
+}
+
+const game = {
+  scripts: 'src/api/**/*.js',
+  assets:  'src/api/**/*.*',
+  ex:      '!src/api/**/*.js',
+  to:      'public/game/'
+}
+
 // Styles
-gulp.task('styles', () => {
-  return gulp.src(path.styles.from)
+gulp.task('styles:site', () => {
+  return gulp.src(site.styles.from)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer('last 2 version'))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest(path.styles.to))
+    .pipe(gulp.dest(site.styles.to))
 });
 
 // Scripts
-gulp.task('scripts', () => {
-  return gulp.src(path.scripts.from)
+gulp.task('scripts:site', () => {
+  return gulp.src(site.scripts.from)
     .pipe(sourcemaps.init())
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
@@ -49,39 +73,82 @@ gulp.task('scripts', () => {
     }))
     .pipe(concat('bundle.js'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(path.scripts.to))
+    .pipe(gulp.dest(site.scripts.to))
 });
 
+gulp.task('scripts:game', () => {
+  return gulp.src(game.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(game.to))
+});
+
+gulp.task('lint:api', function () {
+  gulp.src(api.scripts)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+})
+
+
 // Templates
-gulp.task('templates', () => {
+gulp.task('templates:site', () => {
   var styles = [];
 
-  glob.sync(path.styles.end).forEach(function(file) {
+  glob.sync(site.styles.end).forEach(function(file) {
     styles.push(file.replace("public", ""));
   })
 
-  return gulp.src(path.templates.exclude.concat([path.templates.from]))
+  return gulp.src(site.templates.ex.concat([site.templates.from]))
     .pipe(jade({
       pretty: false,
       locals: {styles: styles}
     }))
-    .pipe(gulp.dest(path.templates.to))
+    .pipe(gulp.dest(site.templates.to))
 });
+
+
+// Assets
+gulp.task('assets:site', function() {
+  gulp.src(site.assets.ex.concat([site.assets.from]))
+    .pipe(gulp.dest(site.assets.to));
+});
+
+gulp.task('assets:game', function() {
+  gulp.src([game.ex, game.assets])
+    .pipe(gulp.dest(game.to));
+});
+
 
 // Watch
 gulp.task('watch', () => {
-  gulp.watch(path.styles.from, ['styles']);
-  gulp.watch(path.scripts.from, ['scripts']);
-  gulp.watch(path.templates.from, ['templates']);
-  nodemon({ script: 'app.js', ext: 'js'}).on('restart', () => {
-    console.log('restarted');
+  gulp.watch(site.styles.from, ['styles:site']);
+  gulp.watch(site.scripts.from, ['scripts:site']);
+  gulp.watch(site.templates.from, ['templates:site']);
+  gulp.watch(site.assets.from, ['assets:site']);
+
+  nodemon({
+    script: 'app.js',
+    watch: [api.scripts],
+    ext: 'js',
+    tasks: ['lint:api']
   })
 });
+
 
 // CLR
 gulp.task('clr', () => {
   process.stdout.write('\x1Bc');
 });
 
+
 // Default task
-gulp.task('default', ['clr', 'styles', 'scripts', 'templates', 'watch']);
+gulp.task('site', ['styles:site', 'scripts:site', 'templates:site', 'assets:site']);
+gulp.task('api', ['lint:api']);
+gulp.task('game', ['assets:game', 'scripts:game']);
+gulp.task('default', ['clr', 'site', 'api', 'watch']);

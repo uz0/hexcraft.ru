57c1f16a3dd48d48254f4a12d8e7ba5def53efeb
Roman <efim1382@gmail.com>
2016-04-10 20:07:50 +0300

    #18 create filter date on news page

--- src/site/javascripts/newsDate.filter.js
+++ src/site/javascripts/newsDate.filter.js
@@ -1,8 +1,29 @@
 'use strict';
 
-export default function newsDateFilter() {
+export default function newsDateFilter($filter) {
   return function(input) {
-    console.log(input);
-    return input;
+
+    if (input == "") return "";
+
+    var inputDay = $filter('date')(new Date(input), 'dd');
+    var inputDate = $filter('date')(new Date(input), 'MM:yyyy');
+    var inputFullDate = $filter('date')(new Date(input), 'dd:MM:yyyy');
+
+    var currentFullDate = $filter('date')(new Date(), 'dd:MM:yyyy');
+    var currentDate = $filter('date')(new Date(), 'MM:yyyy');
+    var currentDay = $filter('date')(new Date(), 'dd');
+
+    var yesterday = (currentDay - 1).toString()
+    yesterday = (yesterday.length === 1) ? '0' + yesterday : yesterday;
+
+    if (currentFullDate === inputFullDate) {
+      return "Сегодня"
+    }
+
+    if (yesterday === inputDay && currentDate === inputDate) {
+      return "Вчера";
+    }
+
+    return inputFullDate;
   }
 }

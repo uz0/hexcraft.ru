eff361ce715bdc0063d12ce31269cd6d0ec290c5
versus <dcversus@gmail.com>
2016-04-21 17:39:41 +0300

    #39 site index scene (interactive background)

--- src/site/javascripts/app.js
+++ src/site/javascripts/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 import newsCtrl from './news.ctrl.js';
+import indexCtrl from './index.ctrl.js';
 import newsDateFilter from './newsDate.filter.js';
 
 angular.module('hexcraft', [])
        .controller('newsCtrl', newsCtrl)
+       .controller('indexCtrl', indexCtrl)
        .filter('newsDateFilter', newsDateFilter);
---
+++ src/site/javascripts/index.ctrl.js
@@ -0,0 +1,37 @@
+'use strict';
+
+export default class indexCtrl {
+  constructor() {
+    this.renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight);
+    this.renderer.backgroundColor = 0xFFFFFF;
+    document.body.appendChild(this.renderer.view);
+
+    this.stage = new PIXI.Stage();
+
+    this.displacementSprite = PIXI.Sprite.fromImage("/images/displacementMap.jpg");
+    this.displacementFilter = new PIXI.filters.DisplacementFilter(this.displacementSprite);
+
+    var bg = PIXI.Sprite.fromImage("/images/background.jpg");
+    this.stage.addChild(bg);
+    this.stage.filters = [this.displacementFilter];
+    this.stage.interactive = true;
+    this.stage.on('mousemove', this.onMove.bind(this))
+    this.tick = 30;
+    this.loop();
+  }
+
+  onMove() {
+    this.tick -= 2;
+  }
+
+
+  loop () {
+    this.tick += 0.5;
+    // if(this.tick > 100) this.tick = 0;
+    this.displacementFilter.scale.x = this.tick;
+    this.displacementFilter.scale.y = this.tick;
+
+    window.requestAnimationFrame(this.loop.bind(this));
+    this.renderer.render(this.stage);
+  }
+}

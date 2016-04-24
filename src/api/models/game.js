092ba4a074364ecb1f4289b5917d19bf92831f14
versus <dcversus@gmail.com>
2016-04-24 23:56:44 +0300

    #29 game methods some rework

--- src/api/models/game.js
+++ src/api/models/game.js
@@ -1,17 +1,11 @@
 'use strict';
 
-
-
 module.exports = function(sequelize, DataTypes) {
   var Game = sequelize.define('Game', {
     levelId: DataTypes.INTEGER,
     player1: DataTypes.INTEGER,
     player2: DataTypes.INTEGER,
     stage: DataTypes.ENUM('Not started', 'Started', 'Over')
-  }, {
-    classMethods: {
-
-    }
   });
 
   return Game;

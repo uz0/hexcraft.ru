8dc3565f458c23e4ac457f1af80cc744c45cb0b2
versus <dcversus@gmail.com>
2016-04-24 23:52:33 +0300

    #38 isAdmin / isAuthed middlewares

---
+++ src/api/middlewares/isAdmin.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const models = require('../models');
+
+module.exports = function(req, res, next) {
+  const token = req.body.token;
+
+  if(!token) {
+    let error = new Error('token not set');
+    error.status = 400;
+    return next(error);
+  }
+
+  models.Token.findOne({
+    include: [models.User],
+    where: {
+      token: token
+    }
+  }).then(result => {
+
+    if(!result) {
+      let error = new Error('invalid token');
+      error.status = 400;
+      return next(error);
+    }
+
+    if(!result.User.admin) {
+      let error = new Error('access denied');
+      error.status = 400;
+      return next(error);
+    }
+
+    next();
+  });
+};
---
+++ src/api/middlewares/isAuthed.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const models = require('../models');
+
+module.exports = function(req, res, next) {
+  const token = req.body.token;
+
+  if(!token) {
+    let error = new Error('token not set');
+    error.status = 400;
+    return next(error);
+  }
+
+  models.Token.findOne({
+    where: {
+      token: token
+    }
+  }).then(result => {
+    if(!result) {
+      let error = new Error('invalid token');
+      error.status = 400;
+      return next(error);
+    }
+
+    next();
+  });
+};

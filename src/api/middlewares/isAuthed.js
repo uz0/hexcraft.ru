fbdff0e88e5a1bbb92a3b9ab1baafba8d32799e3
Seva <sevamaltsev@gmail.com>
2016-04-26 11:59:34 +0300

    Indentation correction

--- src/api/middlewares/isAuthed.js
+++ src/api/middlewares/isAuthed.js
@@ -4,28 +4,23 @@ const models = require('../models');
 
 module.exports = function(req, res, next) {
   const token = req.body.token;
-
-  if(!token) {
+  if (!token) {
     let error = new Error('token not set');
     error.status = 400;
     return next(error);
   }
-
   models.Token.findOne({
     where: {
       token: token
     },
-    include:[models.User]
+    include: [models.User]
   }).then(result => {
-    if(!result){
-    let error = new Error('invalid token');
-    error.status = 400;
-    return next(error);
-  }
-
-  req.user = result.User;
-
-
-  next();
+    if (!result) {
+      let error = new Error('invalid token');
+      error.status = 400;
+      return next(error);
+    }
+    req.user = result.User;
+    next();
   });
 };

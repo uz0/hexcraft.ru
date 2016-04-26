3efb4ea8db63854ddcd43d2eb5eb1e10634aee2f
Seva <sevamaltsev@gmail.com>
2016-04-26 12:52:53 +0300

    #50 Added link to gamedata table

--- src/api/models/game.js
+++ src/api/models/game.js
@@ -1,12 +1,18 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-  var Game = sequelize.define('Game', {
+  let Game = sequelize.define('Game', {
     levelId: DataTypes.INTEGER,
     player1: DataTypes.INTEGER,
     player2: DataTypes.INTEGER,
     stage: DataTypes.ENUM('Not started', 'Started', 'Over')
-  });
-
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Game.hasMany(models.GameData, { foreignKey: 'gameId' });
+      }
+    }
+  })
+;
   return Game;
 };

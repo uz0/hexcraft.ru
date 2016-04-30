1d06cda5a020e76f714d76bff4a95d0c14aa69b1
versus <dcversus@gmail.com>
2016-04-30 21:19:03 +0300

    #52 editor auth

--- src/site/javascripts/editor.ctrl.js
+++ src/site/javascripts/editor.ctrl.js
@@ -4,8 +4,18 @@ export default class editorCtrl {
   static $inject = ['$http']; // jshint ignore:line
 
   constructor($http) {
+    this.$http = $http;
     // $http.get('/api/news').then((res)=>{
     //   this.list = res.data;
     // });
+  }
+
+  auth() {
+    this.$http.post('/api/auth', {
+      login: this.login,
+      password: this.password
+    }).then(res => {
+      console.log(res)
+    });
   }
 }

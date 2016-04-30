eb0ccb13f9ad126f228c2ecfc43c0a836b9d1d96
versus <dcversus@gmail.com>
2016-04-30 17:12:54 +0300

    admin user seeder

--- seeders/20160425112256-users.js
+++ seeders/20160425112256-users.js
@@ -7,6 +7,12 @@ module.exports = {
       password: '$2a$10$3.YGiknAFfM0FvzdPz2OYO08sCLBXKtTr71i9EXD2b5SvOy5WM/py',
       createdAt: new Date(),
       updatedAt: new Date()
+    },{
+      username: 'admin',
+      password: '$2a$10$3.YGiknAFfM0FvzdPz2OYOdm9nfv4bl6SPGzWg0lwJpxT3jRDa.xO',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      admin: true
     }]);
   }
 };

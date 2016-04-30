b74c7a1927bf93427e5c31fc9eff33b5a77e5eab
versus <dcversus@gmail.com>
2016-04-30 19:24:04 +0300

    #58 map editor beginning

--- src/site/javascripts/app.js
+++ src/site/javascripts/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
+import editorCtrl from './editor.ctrl.js';
 import newsCtrl from './news.ctrl.js';
 import indexCtrl from './index.ctrl.js';
 import newsDateFilter from './newsDate.filter.js';
 
 angular.module('hexcraft', [])
+       .controller('editorCtrl', editorCtrl)
        .controller('newsCtrl', newsCtrl)
        .controller('indexCtrl', indexCtrl)
        .filter('newsDateFilter', newsDateFilter);
---
+++ src/site/javascripts/editor.ctrl.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export default class editorCtrl {
+  static $inject = ['$http']; // jshint ignore:line
+
+  constructor($http) {
+    // $http.get('/api/news').then((res)=>{
+    //   this.list = res.data;
+    // });
+  }
+}

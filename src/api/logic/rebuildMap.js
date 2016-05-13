859a9e46d29e0f789c6f37009671aebc06740b2a
Seva <sevamaltsev@gmail.com>
2016-05-13 12:37:08 +0300

    #73 #75 revision and game logic changes

--- src/api/logic/rebuildMap.js
+++ src/api/logic/rebuildMap.js
@@ -17,14 +17,15 @@ module.exports = function(game, step, emit) {
 
   // If moved by one, capture the sorrounding enemy cells and copy to origin cell
   let changed = [];
-  if (!inRadius) {
-    neighbors.forEach(cell => {
-      if (cell.cellstate !== 'empty' && cell.cellstate !== owner) {
-        cell.cellstate = owner;
-        changed.push(cell);
-      }
-    });
-  } else {
+
+  neighbors.forEach(cell => {
+    if (cell.cellstate !== 'empty' && cell.cellstate !== owner) {
+      cell.cellstate = owner;
+      changed.push(cell);
+    }
+  });
+
+  if (inRadius) {
     Hex.findByIndex(game.Map.MapData, step.old.i, step.old.j).cellstate = owner;
     emit({
       name: 'chip',
@@ -42,25 +43,4 @@ module.exports = function(game, step, emit) {
     });
   }
 
-  // change owners (find current neighbors)
-  // emit({
-  //   name: 'owner',
-  //   data: [{
-  //     i: 1,
-  //     j: 1
-  //   }, {
-  //
-  // }]
-  // });
-
-  // check if in radius (from old) and create new chip
-  // emit({
-  //   name: 'chip',
-  //   data: {
-  //     i: step.old.i,
-  //     j: step.old.j
-  //   }
-  // });
-
-  return game.Map.MapData;
 };

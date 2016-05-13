257f3ee4bc0dea95f8657dd5917615b9a0ba55b1
Seva <sevamaltsev@gmail.com>
2016-05-13 12:17:30 +0300

    #73 #75 revision and game logic changes

--- src/api/logic/rebuildMap.js
+++ src/api/logic/rebuildMap.js
@@ -6,37 +6,33 @@ module.exports = function(game, step, emit) {
   let owner = (game.player1.id === step.userId) ? 'player1' : 'player2';
 
   // rebuild mapData using step (without checks)
-  let stepsNeighbors = Hex.findNeighbors(game.Map.MapData, step.current.i, step.current.j);
-  let changed = [];
-  let inRadius = false;
+
+  let neighbors = Hex.findNeighbors(game.Map.MapData, step.current.i, step.current.j);
 
   Hex.findByIndex(game.Map.MapData, step.current.i, step.current.j).cellstate = owner;
+  Hex.findByIndex(game.Map.MapData, step.old.i, step.old.j).cellstate = 'empty';
 
   // Check to see if moved by one
-  stepsNeighbors.forEach(cell => {
-    if (cell.i === step.old.i && cell.j === step.old.j) {
-      inRadius = true;
-    }
-  });
+  let inRadius = Hex.findByIndex(neighbors, step.old.i, step.old.j);
 
   // If moved by one, capture the sorrounding enemy cells and copy to origin cell
-  if (inRadius) {
-    Hex.findByIndex(game.Map.MapData, step.old.i, step.old.j).cellstate = 'owner';
-    stepsNeighbors.forEach(cell => {
-      if (cell.cellstate !== 'empty' && cell.cellstate !== 'owner') {
+  let changed = [];
+  if (!inRadius) {
+    neighbors.forEach(cell => {
+      if (cell.cellstate !== 'empty' && cell.cellstate !== owner) {
         cell.cellstate = owner;
         changed.push(cell);
       }
     });
-    emit({
-     name: 'chip',
-     data: {
-       i: step.old.i,
-       j: step.old.j
-     }
-   });
   } else {
-    Hex.findByIndex(game.Map.MapData, step.old.i, step.old.j).cellstate = 'empty';
+    Hex.findByIndex(game.Map.MapData, step.old.i, step.old.j).cellstate = owner;
+    emit({
+      name: 'chip',
+      data: {
+        i: step.old.i,
+        j: step.old.j
+      }
+    });
   }
 
   if (changed) {

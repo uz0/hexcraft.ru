3c38ac037beda0c544bbd2b4033b38acc1d6882a
versus <dcversus@gmail.com>
2016-05-16 01:06:09 +0300

    new map for play

---
+++ src/api/seeders/testSuite.js
@@ -0,0 +1,420 @@
+
+      'use strict';module.exports = {up: function (q, s, d) {q.bulkInsert('Maps', [{
+        description: 'testSuite',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]).then(id => {q.bulkInsert('MapData',[{
+        MapId: id,
+        i: 4,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 2,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 2,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 2,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 4,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 4,
+        j: 2,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 4,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 4,
+        j: 4,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 4,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 6,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 4,
+        j: 6,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 6,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 2,
+        j: 4,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 1,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 1,
+        j: 2,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 1,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 1,
+        j: 6,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 6,
+        cellstate: 'player1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 5,
+        cellstate: 'player1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 1,
+        j: 4,
+        cellstate: 'player1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 4,
+        cellstate: 'player1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 3,
+        cellstate: 'player1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 2,
+        cellstate: 'player1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 0,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 7,
+        j: 4,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 8,
+        j: 4,
+        cellstate: 'player2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 9,
+        j: 4,
+        cellstate: 'player2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 8,
+        j: 3,
+        cellstate: 'player2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 7,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 9,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 8,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 8,
+        j: 2,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 9,
+        j: 2,
+        cellstate: 'player2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 8,
+        j: 6,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 7,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 8,
+        j: 5,
+        cellstate: 'player2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 9,
+        j: 6,
+        cellstate: 'player2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 6,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 4,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 5,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 2,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 1,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 7,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 2,
+        j: 0,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 4,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 6,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 2,
+        j: 3,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 3,
+        j: 4,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        MapId: id,
+        i: 2,
+        j: 5,
+        cellstate: 'empty',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]).then(()=>{d();});});}};
+    

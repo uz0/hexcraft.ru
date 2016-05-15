613c97dc2848a4ffb762c4a64c240ec5cfcd02c4
versus <dcversus@gmail.com>
2016-05-16 01:05:24 +0300

    #93 update news date format

--- src/site/javascripts/newsDate.filter.js
+++ src/site/javascripts/newsDate.filter.js
@@ -6,11 +6,11 @@ export default function newsDateFilter($filter) {
     if (input === ''){ return ''; }
 
     var inputDay = $filter('date')(new Date(input), 'dd');
-    var inputDate = $filter('date')(new Date(input), 'MM:yyyy');
-    var inputFullDate = $filter('date')(new Date(input), 'dd:MM:yyyy');
+    var inputDate = $filter('date')(new Date(input), 'longDate');
+    var inputFullDate = $filter('date')(new Date(input), 'longDate');
 
-    var currentFullDate = $filter('date')(new Date(), 'dd:MM:yyyy');
-    var currentDate = $filter('date')(new Date(), 'MM:yyyy');
+    var currentFullDate = $filter('date')(new Date(), 'longDate');
+    var currentDate = $filter('date')(new Date(), 'longDate');
     var currentDay = $filter('date')(new Date(), 'dd');
 
     var yesterday = (currentDay - 1).toString();

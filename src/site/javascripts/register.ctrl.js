bcfaf98e272f54410e906482a855b23b4cc67fc1
Danil Stoyanov <danya.bright@yandex.ru>
2016-05-17 23:41:47 +0300

    #89 making frontend validation for the register form

--- src/site/javascripts/register.ctrl.js
+++ src/site/javascripts/register.ctrl.js
@@ -5,13 +5,25 @@ export default class registerCtrl {
 
   constructor($http) {
     this.$http = $http;
-	}
+  }
 
-	register() {
-		this.$http.post('/api/users', {
-			username: this.username,
-			password: this.password
-		});
-	}
+  register() {
+    this.$http.post('/api/users', {
+      username: this.username,
+      password: this.password
+    })
+    .then(res => {
+      this.responseData = 'Вы успешно зарегистрировались';
+      this.reset();
+    })
+    .catch(res => {
+      this.responseData = res.data.error;
+      this.reset();
+    });
+  }
+  reset() {
+      this.username = '';
+      this.password = '';
+  }
 
 }

2729c2e6285deedbc0f11949b223fcecefb3f49c
versus <dcversus@gmail.com>
2016-05-18 22:32:35 +0300

    #98 water background on game

--- src/game/application.js
+++ src/game/application.js
@@ -4,9 +4,15 @@ import Load from './application/load/load.js';
 
 class Hexcraft {
   constructor() {
+    this.tick = 100;
+
     this.renderer = new PIXI.WebGLRenderer();
     this.renderer.backgroundColor = 0xFFFFFF;
     document.body.appendChild(this.renderer.view);
+
+    this.water = this.setupWater();
+    this.container = new PIXI.Container();
+    this.container.addChild(this.water);
 
     this.setStage(Load);
     this.loop();
@@ -15,10 +21,43 @@ class Hexcraft {
     window.addEventListener('orientationchange', this.updateScale.bind(this));
   }
 
+  setupWater(){
+    let water = new PIXI.Container();
+    // water.scale  = {
+    //   x: 2,
+    //   y: 2
+    // };
+
+    let displacementSprite = PIXI.Sprite.fromImage('/images/displacementMap.jpg');
+    let displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+    displacementFilter.scale = {
+      x: 100,
+      y: 100
+    };
+
+    water.bg = PIXI.extras.TilingSprite.fromImage('/images/water.png', window.innerWidth, window.innerHeight);
+    water.addChild(water.bg);
+    water.filters = [displacementFilter];
+
+    return water;
+  }
+
+  updateWater(){
+    this.tick += 0.005;
+
+    this.water.bg.tileScale.x = 2 + Math.cos(this.tick);
+    this.water.bg.tileScale.y = 4 + Math.sin(this.tick);
+
+    this.water.bg.tilePosition.x += Math.cos(this.tick);
+  }
+
   setStage (Stage, argument) {
+    if(this.stage) {
+      this.container.removeChildren(1);
+    }
+
     this.stage = new Stage(argument);
-    this.stage.originalWidth = this.stage.width;
-    this.stage.originalHeight = this.stage.height;
+    this.container.addChildAt(this.stage, 1);
     this.updateScale();
   }
 
@@ -34,18 +73,19 @@ class Hexcraft {
 
     this.stage.scale.x = this.stage.scale.y = ratio;
     this.renderer.resize(newWidth, newHeight);
-
+    this.water.bg.width = newWidth;
+    this.water.bg.height = newHeight;
 
     let newAspectRatio = newWidth / newHeight;
 
     this.stage.y = 0;
     this.stage.x = 0;
 
-    console.group('stage - ', this.stage);
-    console.log('ratio:', ratio, ' : ', newAspectRatio);
-    console.log('width:', newWidth, ' : ', this.stage.width);
-    console.log('height:', newHeight, ' : ', this.stage.height);
-    console.groupEnd();
+    // console.group('stage - ', this.stage);
+    // console.log('ratio:', ratio, ' : ', newAspectRatio);
+    // console.log('width:', newWidth, ' : ', this.stage.width);
+    // console.log('height:', newHeight, ' : ', this.stage.height);
+    // console.groupEnd();
 
     if (newAspectRatio > aspectRatio) {
       this.stage.x = (newWidth - this.stage.width) / 2;
@@ -61,8 +101,10 @@ class Hexcraft {
       return;
     }
 
+    this.updateWater();
+
     this.stage.update();
-    this.renderer.render(this.stage);
+    this.renderer.render(this.container);
   }
 }
 

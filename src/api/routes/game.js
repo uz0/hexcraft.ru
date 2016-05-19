89811cb9373ff4ebf529a981794bcb7cd0257653
versus <dcversus@gmail.com>
2016-05-19 12:59:20 +0300

    #86 some game apidoc updates

--- src/api/routes/game.js
+++ src/api/routes/game.js
@@ -6,9 +6,98 @@ const express = require('express');
 const router = module.exports = express.Router();
 const sse = require('server-sent-events');
 
+/**
+ * @apiDefine gameDetails
+ *
+ * @apiSuccess {String}       id                  UUID of game.
+ * @apiSuccess {String}       stage               Stage the game is in. Can be "not started", "started", "over player1" and "over player2".
+ * @apiSuccess {Object}       player1             First player details
+ * @apiSuccess {Object} player2             Depending on situation, can be second player details or Null (if game not started)
+ * @paiSuccess {Object}       map                 Game's map data, including MapData (array of valid cells in the map).
+ * @paiSuccess {Number}       map.id
+ * @paiSuccess {String}       map.description
+ * @paiSuccess {Date}         map.createdAt
+ * @paiSuccess {Date}         map.updatedAt
+ * @paiSuccess {Object[]}     map.MapData
+ * @paiSuccess {Number}       map.MapData.id
+ * @paiSuccess {String}       map.MapData.cellstate
+ * @paiSuccess {Number}       map.MapData.i
+ * @paiSuccess {Number}       map.MapData.j
+ * @paiSuccess {Date}         map.MapData.createdAt
+ * @paiSuccess {Date}         map.MapData.updatedAt
+ * @paiSuccess {Number}       map.MapData.MapId
+ * @apiSuccess {Object[]}     gameSteps           Contain all game steps with player id
+ * @apiSuccess {Object}       gameSteps.current
+ * @apiSuccess {Number}       gameSteps.current.i
+ * @apiSuccess {Number}       gameSteps.current.j
+ * @apiSuccess {Object}       gameSteps.old
+ * @apiSuccess {Number}       gameSteps.old.i
+ * @apiSuccess {Number}       gameSteps.old.j
+ *
+ * @apiSuccessExample Without second player
+    {
+
+      "id": "630444ed-037e-43b0-a558-32eb8c5a7278",
+      "stage": "not started",
+      "Map": {
+        "id": 3,
+        "description": "11111",
+        "MapData": [{
+          "i": 4,
+          "j": 0,
+          "cellstate": "empty",
+          "MapId": 3
+        }]
+      },
+      "player1": {
+        "id": 3,
+        "username": "admin",
+        "admin": true
+      },
+      "player2": null,
+      "gameSteps": []
+    }
+ *
+ * @apiSuccessExample With second player and game steps
+    {
+      "id": "630444ed-037e-43b0-a558-32eb8c5a7278",
+      "stage": "started",
+      "Map": {
+        "id": 3,
+        "description": "11111",
+        "MapData": [{
+          "i": 4,
+          "j": 0,
+          "cellstate": "empty",
+          "MapId": 3
+        }]
+      },
+      "player1": {
+        "id": 3,
+        "username": "admin",
+        "admin": true
+      },
+      "player2": {
+        "id": 2,
+        "username": "test",
+        "admin": null
+      }
+      "gameSteps":[{
+          "current":{
+             "i":2,
+             "j":0
+          },
+          "old":{
+             "i":1,
+             "j":0
+          },
+          "userId":2
+       }]
+    }
+ */
 
 /**
- * @api {get} /games get List all games
+ * @api {get} /games All games
  * @apiName getGames
  * @apiGroup Game
  *
@@ -24,67 +113,11 @@ router.get('/', function(req, res) {
  * @api {post} /games Start game
  * @apiName startGame
  * @apiGroup Game
- * 
- * @apiDescription Starts a game for the user. Finds games that don't have a second player, or, if none were found, creates a new one and returns the instance. 
- * @apiSuccess {Number} id UUID of game.
- * @apiSuccess {String} stage Staage the game is in. Can be "not started", "started", "over".
- * @apiSuccess {Object} player1 Depending on situation, can be either your info or your oponent's.
- * @apiSuccess {Object} player2 Depending on situation, can be either your info or your oponent's.
- * @paiSuccess {Object} map Game's map data, including MapData (array of valid cells in the map).
- * @apiSuccess {Array} gameSteps steps that were already taken in the game. Used to recreate the map.
- * @apiSuccessExample Create game:
-    {
-
-      "id": "630444ed-037e-43b0-a558-32eb8c5a7278",
-      "stage": "not started",
-      "Map": {
-        "id": 3,
-        "description": "11111",
-        "MapData": [{
-            "i": 4,
-            "j": 0,
-            "cellstate": "empty",
-            "MapId": 3
-          },
-          ...
-        ]
-      },
-      "player1": {
-        "id": 3,
-        "username": "admin",
-        "admin": true
-      },
-      "player2": null,
-      "gameSteps": []
-    }
- * @apiSuccessExample Connect to a game:
-    {
-      "id": "630444ed-037e-43b0-a558-32eb8c5a7278",
-      "stage": "started",
-      "Map": {
-        "id": 3,
-        "description": "11111",
-        "MapData": [{
-            "i": 4,
-            "j": 0,
-            "cellstate": "empty",
-            "MapId": 3
-          },
-          ...
-        ]
-      },
-      "player1": {
-        "id": 3,
-        "username": "admin",
-        "admin": true
-      },
-      "player2": {
-        "id": 2,
-        "username": "test",
-        "admin": null
-      }
-      "gameSteps": []
-    }
+ *
+ * @apiDescription Starts a game for the user. Finds games that don't have
+ * a second player or if none were found, creates a new one and returns the instance.
+ *
+ * @apiUse gameDetails
  */
 
 router.post('/', isAuthed, function(req, res) {
@@ -95,12 +128,13 @@ router.post('/', isAuthed, function(req, res) {
 
 
 /**
- * @api {get} /games/:id
- * @apiName gameUpdate
+ * @api {get} /games/:id Get game
+ * @apiName getGame
  * @apiGroup Game
  *
  * @apiParam {Number} id Game's Id
- * @apiSuccess {Object} updatedFields hexes that have changed
+ *
+ * @apiUse gameDetails
  */
 
 router.get('/:id', function(req, res) {
@@ -114,13 +148,12 @@ router.get('/:id', function(req, res) {
 
 
 /**
- * @api {post} /games/:id
+ * @api {post} /games/:id Game step
  * @apiName gameStep
  * @apiGroup Game
  *
- * @apiDescription Send updeted fields. Only a 400 status will always return. Updates will be SSE'd at /games/:id/loop
+ * @apiDescription Send updeted fields. Updates will be SSE'd at /games/:id/loop
  * @apiParam {Number} id Game's Id
- * @apiParam {Object} updatedFields Fields that have changed
  */
 
 router.post('/:id', isAuthed, function(req, res, next) {
@@ -148,11 +181,17 @@ router.post('/:id', isAuthed, function(req, res, next) {
 
 
 /**
- * @api {get} /games/:id/loop
+ * @api {get} /games/:id/loop Game loop
  * @apiName gameLoop
  * @apiGroup Game
  *
- * @apiDescription This loop uses Server Sent Events to push updates to both clients when a move happens. Only valid moves are pushed. Several events can happen during one move, i.e. move, clone, capture action. All will be sent seperately: one action, one event.
+ * @apiDescription This loop uses Server Sent Events to push updates to both clients
+ * when a move happens. Only valid moves are pushed. Several events can happen during
+ * one move, i.e. move, clone, capture action. All will be sent seperately:
+ * one action, one event.
+ *
+ * @apiParam {Number} id Game's Id
+ *
  * @apiSuccessExample Step:
  {
    "event": "step",
@@ -173,7 +212,6 @@ router.post('/:id', isAuthed, function(req, res, next) {
      "admin": null
    }
  }
- * @apiParam {Number} id Game's Id
  */
 
 router.get('/:id/loop', sse, function(req, res) {
@@ -187,11 +225,12 @@ router.get('/:id/loop', sse, function(req, res) {
 
 
 /**
- * @api {post} /games/:id/surrender
+ * @api {post} /games/:id/surrender Surrender
  * @apiName surrender
  * @apiGroup Game
  *
  * @apiDescription To quit the game, post to here, and you will surrender and the game will end
+ *
  * @apiParam {Number} id Game's Id
  */
 

b7838384cfb7a0eb4ffd17e536456869156eeb4f
versus <dcversus@gmail.com>
2016-05-22 23:05:34 +0300

    #104 old loading scene correction

--- src/game/application/load/load.js
+++ src/game/application/load/load.js
@@ -3,12 +3,12 @@
 import resources from '../../resources.json';
 import hexcraft from '../../application.js';
 import Auth from '../auth/auth.js';
+import GUI from '../gui.js';
 
 // load all from resources.json
 class Loader extends PIXI.loaders.Loader {
   constructor() {
     super();
-
     let Resource = PIXI.loaders.Resource;
     Resource.setExtensionLoadType('wav', Resource.LOAD_TYPE.XHR);
     Resource.setExtensionXhrType('wav', Resource.XHR_RESPONSE_TYPE.BUFFER);
@@ -24,6 +24,10 @@ export default class Load extends PIXI.Container {
   constructor() {
     super();
 
+    // Create empty GUI layout for fit scene game to width and height
+    this.GUI = new GUI([]);
+    this.addChild(this.GUI);
+
     // init load
     var loader = new Loader();
     loader.once('complete', this.loaded.bind(this));
@@ -34,11 +38,11 @@ export default class Load extends PIXI.Container {
     var logo = PIXI.Texture.fromImage('/game/resources/logo.svg');
     this.logo = new PIXI.Sprite(logo);
 
-    this.logo.position.x = window.innerWidth/2;
-    this.logo.position.y = window.innerHeight/2;
+    this.logo.position.x = 400;
+    this.logo.position.y = 320;
     this.logo.anchor.set(0.5);
     this.logo.scale.set(0.5);
-    this.logo.alpha = 0;
+    // this.logo.alpha = 0;
 
     this.addChild(this.logo);
   }
@@ -57,16 +61,16 @@ export default class Load extends PIXI.Container {
       resource.blobUrl = this.generateBlobUrl(new window.DataView(resource.data), 'audio/wav');
     }
 
-    this.logo.alpha = loader.progress / 100;
+    // this.logo.rotation = loader.progress / 100;
   }
 
   generateBlobUrl(text, type) {
-      let DOMURL = window.self.URL || window.self.webkitURL || window.self;
-      let svg = new window.Blob([text], {
-        type: type
-      });
+    let DOMURL = window.self.URL || window.self.webkitURL || window.self;
+    let svg = new window.Blob([text], {
+      type: type
+    });
 
-      return DOMURL.createObjectURL(svg);
+    return DOMURL.createObjectURL(svg);
   }
 
   update(){}

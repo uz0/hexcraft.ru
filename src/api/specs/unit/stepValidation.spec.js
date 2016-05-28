71ceb6635fad2e0cacf95a74be047eb5e46e94fc
Anton <er.ant14@yandex.ru>
2016-05-28 23:27:35 +0300

    #91 stepValidation refactoring

--- src/api/specs/unit/stepValidation.spec.js
+++ src/api/specs/unit/stepValidation.spec.js
@@ -61,6 +61,8 @@ describe('stepValidation', () => {
     });
   });
 
+
+
   it('step order', () => {
     stepValidation(data, {
       userId: 1
@@ -69,29 +71,168 @@ describe('stepValidation', () => {
     });
   });
 
+
+
   it('check owner', () => {
-    stepValidation(data, step, (error, code) => {
+    stepValidation( {
+      player1: {
+        id: 1
+      },
+      player2: {
+        id: 2
+      },
+      gameSteps:[{
+        current:{
+          i: 2,
+          j: 0
+        },
+        old:{
+          i: 1,
+          j: 0
+        },
+        userId: 2
+      }, {
+        current:{
+          i: 5,
+          j: 0
+        },
+        old:{
+          i: 3,
+          j: 0
+        },
+        userId: 2
+      }],
+      Map: {
+        MapData: [{
+          i: 1,
+          j: 0,
+          cellstate: 'player2'
+        }]
+      }
+    }, {
+      old: {
+        i: 1,
+        j: 0
+      },
+      userId: 1
+    }, (error, code) => {
       expect(code).to.be(3);
     });
   });
 
+
+
   it('check for collisions', () => {
-    stepValidation(data, step, (error, code) => {
+    stepValidation( {
+      player1: {
+        id: 1
+      },
+      player2: {
+        id: 2
+      },
+      gameSteps:[{
+        current:{
+          i: 2,
+          j: 0
+        },
+        old:{
+          i: 1,
+          j: 0
+        },
+        userId: 2
+      }, {
+        current:{
+          i: 5,
+          j: 0
+        },
+        old:{
+          i: 3,
+          j: 0
+        },
+        userId: 2
+      }],
+      Map: {
+        MapData: [{
+          i: 1,
+          j: 0,
+          cellstate: 'player2'
+        }, {
+          i: 5,
+          j: 0,
+          cellstate: 'player2'
+        }]
+      }
+    }, {
+      old: {
+        i: 1,
+        j: 0
+      },
+      current: {
+        i: 5,
+        j: 0
+      },
+      userId: 2
+    }, (error, code) => {
       expect(code).to.be(4);
     });
   });
 
+
+
   it('check for distance', () => {
-    stepValidation(data, {
+    stepValidation( {
+      player1: {
+        id: 1
+      },
+      player2: {
+        id: 2
+      },
+      gameSteps:[{
+        current:{
+          i: 2,
+          j: 0
+        },
+        old:{
+          i: 1,
+          j: 0
+        },
+        userId: 2
+      }, {
+        current:{
+          i: 5,
+          j: 0
+        },
+        old:{
+          i: 3,
+          j: 0
+        },
+        userId: 2
+      }],
+      Map: {
+        MapData: [{
+          i: 1,
+          j: 0,
+          cellstate: 'player2'
+        }, {
+          i: 5,
+          j: 0,
+          cellstate: 'empty'
+        }, {
+          i: 10,
+          j: 0,
+          cellstate: 'empty'
+        }]
+      }
+    }, {
       old: {
-        i: 4,
+        i: 1,
         j: 0
       },
       current: {
         i: 10,
-        j: 6
+        j: 0
       },
-      userId: 3
+      userId: 2
     }, (error, code) => {
       expect(code).to.be(5);
     });

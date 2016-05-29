56541bf3f8cda3bbc805f7bc5a4efec6a8c6877c
Seva <sevamaltsev@gmail.com>
2016-05-29 12:36:21 +0300

    changes for heroku

--- src/api/models.js
+++ src/api/models.js
@@ -7,7 +7,17 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV;
 const configFile = require('./configuration.json');
 const config = (configFile[env]) ? configFile[env] : configFile.development;
-const sequelize = new Sequelize(config.database || process.env.database, config.username || process.env.username, config.password || process.env.password, config);
+let sequelize;
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+    dialect: 'mysql',
+    protocol: 'mysql',
+    port: match[4],
+    host: match[3]
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 var db = module.exports = {};
 
 fs.readdirSync(path.join(__dirname, 'models')).forEach(file => {

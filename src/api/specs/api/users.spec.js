b12431210bcf0f278b822cbd6e6d8f036923f55b
Anton <er.ant14@yandex.ru>
2016-06-04 23:47:41 +0300

    #91 users apitest refactoring

--- src/api/specs/api/users.spec.js
+++ src/api/specs/api/users.spec.js
@@ -4,14 +4,12 @@ const expect = require('expect.js');
 const request = require('supertest');
 const app = require('../');
 
-const url = '/api/user';
-const map = {
-  description: 'test',
-  i: 1,
-  j: 3,
-  cellstate: 'empty'
-};
+const url = '/api/users';
+const testUsername = 'testUsername';
+const testPassword = 'testPassword';
+
 let id;
+
 describe('Users', () => {
   before(app.init);
   before(app.auth);
@@ -20,8 +18,8 @@ describe('Users', () => {
     request(app.server)
       .post(url)
       .send({
-        username: 'testUsername',
-        password: 'testPassword',        
+        username: testUsername,
+        password: testPassword    
       })
       .expect(res => {
         id = res.body.id;
@@ -46,8 +44,7 @@ describe('Users', () => {
     request(app.server)
       .get(`${url}/${id}`)
       .expect(res => {
-        expect(res.body.username).to.be.equal('testUsername');
-        expect(res.body.password).to.be.ok();
+        expect(res.body.username).to.be.equal(testUsername);
       })
       .expect(200, done);
   });

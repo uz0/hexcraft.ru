b182b3d4fbe51a0f0f04ad4d668fc2296e70bb7d
versus <dcversus@gmail.com>
2016-06-14 20:43:52 +0300

    #109 online class for guest play

---
+++ src/game/application/board/online.js
@@ -0,0 +1,81 @@
+'use strict';
+
+import http from '../http';
+import Hex from '../../../_shared/game/hex';
+import hexcraft from '../../application';
+import Lobby from '../lobby/lobby';
+
+export default class Online {
+  constructor(gameData, board) {
+    this.id = gameData.id;
+    this.player1 = gameData.player1;
+    this.player2 = gameData.player2;
+    this.Map = gameData.Map;
+    this.board = board;
+
+    this.loop = new window.EventSource(`/api/games/${this.id}/loop`);
+    this.loop.addEventListener('message', this.onEvent.bind(this));
+  }
+
+  onStep(current, old) {
+    let token = window.localStorage.getItem('token');
+    let index = Hex.coordinatesToIndex(current.x, current.y);
+    let oldIndex = Hex.coordinatesToIndex(old.x, old.y);
+
+    http.post(`/api/games/${this.id}`, {
+      token: token,
+      step: {
+        current: index,
+        old: oldIndex
+      }
+    }).catch(() => {
+      // rollback chip, after failure request
+      Hex.findByIndex(this.board.chips.children, index.i, index.j).setPosition(oldIndex);
+    });
+  }
+
+  // EVENTS
+  onEvent(event) {
+    let data = JSON.parse(event.data);
+    this[`${data.event}Event`](data.data, data.player);
+    console.log(data.player);
+  }
+
+  stepEvent(step, player) {
+    let nextUserStep = (player === 'player1') ? 'player2' : 'player1';
+    this.board.changeMode(nextUserStep);
+
+    if(this.board.player === player) {
+      return;
+    }
+
+    Hex.findByIndex(this.board.chips.children, step.old.i, step.old.j).setPosition(step.current);
+
+    Hex.findByIndex(this.Map.MapData, step.old.i, step.old.j).cellstate = 'empty';
+    Hex.findByIndex(this.Map.MapData, step.current.i, step.current.j).cellstate = player;
+  }
+
+  chipEvent(coordinates, player) {
+    this.board.addChip(coordinates.i, coordinates.j, player);
+
+    Hex.findByIndex(this.Map.MapData, coordinates.i, coordinates.j).cellstate = player;
+  }
+
+  ownerEvent(coordinatesArray, player) {
+    coordinatesArray.forEach(coordinates => {
+      Hex.findByIndex(this.board.chips.children, coordinates.i, coordinates.j).changeOwner(player);
+
+      Hex.findByIndex(this.Map.MapData, coordinates.i, coordinates.j).cellstate = player;
+    });
+  }
+
+  overEvent() {
+    this.loop.close();
+    this.board.panel.splash('over', () => {
+      hexcraft.setStage(Lobby, 'Игра окончена');
+    });
+  }
+
+  mapUpdated (){}
+  update (){}
+}

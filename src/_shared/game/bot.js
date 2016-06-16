8d936bc57d653d63f938683a0d197e801d08e5fd
Seva <sevamaltsev@gmail.com>
2016-06-17 00:54:40 +0300

    #111 bot.js takes map and game instance and calls game.step with calculated move

---
+++ src/_shared/game/bot.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const Hex = require('./hex');
+
+function Bot() {};
+
+Bot.user = {
+  "id": -1,
+  "username": "Bot",
+  "admin": null,
+  "createdAt": "1970-01-01T00:00:01.111Z",
+  "updatedAt": "1970-01-01T00:00:01.111Z"
+}
+
+Bot.step = function(game) {
+  let step = Bot.move(game.data);
+  game.step(step, Bot.user);
+}
+
+Bot.toStep = function(cell){
+   let step = {
+    "event": "step",
+    "data": {
+      "current": {
+        "i": cell.bestMove.i,
+        "j": cell.bestMove.j
+      },
+      "old": {
+        "i": cell.data.i,
+        "j": cell.data.j
+      },
+      "userId": Bot.user.id
+    },
+    "user": Bot.user
+  };
+  return step;
+}
+
+Bot.move = function(data) {
+
+  let cells = [];
+
+  data.Map.MapData.forEach(hex => {
+    if (hex.cellstate === 'player2') {
+      let cell = {
+        data: hex
+      }
+      cells.push(cell);
+    }
+  });
+
+  // Calculating all valid moves for all bot cells with a count of captured cells for each move
+  for (var i = 0, i < cells.length, i++) {
+    let cells[i].validSteps = [];
+    Hex.findNeighborsNeighbors(data.Map.MapData, cells[i].data.i, cells[i].data.j).forEach(field => {
+      if (field.cellstate === 'empty') {
+        let count = 0;
+        Hex.findNeighbors(data.Map.MapData, field.i, field.j).forEach(cell => {
+          if (cell.cellstate === 'player1') {
+            count++;
+          }
+        });
+        field.count = count;
+        cells[i].validSteps.push(field);
+      }
+    });
+  }
+
+  for (var i = 0, i < cells.length, i++) {
+    let cells[i].bestMove = cells[i].validSteps[0];
+
+    for (var j = 0, j < cells[i].validSteps.length, j++){
+      if (cells[i].validSteps[j].count>cells[i].bestMove.count){
+        cells[i].bestMove = cells[i].validSteps[j];
+      }
+    }
+  }
+  
+  let bestMove = cells[0];
+  
+  for (var i = 0, i < cells.length, i++) {
+    if (cells[i].bestMove.count>bestMove.count){
+      bestMove = cells[i];
+    }
+  }
+
+  return Bot.toStep(bestMove);
+
+}
+
+module.exports = Bot;
